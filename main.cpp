65efacf64cf283d20b9c8f27329b0d6c7905aff0
icraggs <icraggs@localhost>
2014-02-05 10:50:15 +0000

    Add copyright statement to main program

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2014 IBM Corp.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *    Ian Craggs - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+
 #include "mbed.h"
 #include "EthernetInterface.h"
 #include "C12832_lcd.h"
 
 #include "MQTTPacket.h"
-
 
 DigitalOut myled(LED2);
 C12832_LCD lcd;
@@ -53,13 +68,15 @@ int publish()
     return 0;
 }
 
-int main() {
+int main()
+{
     EthernetInterface eth;
     eth.init(); //Use DHCP
     eth.connect();
     lcd.printf("IP Address is %s\n", eth.getIPAddress());
     
-    while(1) {
+    while(1) 
+    {
         myled = 1;
         publish();
         wait(0.2);
