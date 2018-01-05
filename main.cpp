bd5c68bbbbede2370afcb823b9246078adc79740
ghysbrecht <ghysbrecht@localhost>
2018-01-05 13:03:55 +0000

    First Commit

--- main.cpp
+++ main.cpp
@@ -1,46 +1,8 @@
-/*******************************************************************************
- * Copyright (c) 2014, 2015 IBM Corp.
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * and Eclipse Distribution License v1.0 which accompany this distribution.
- *
- * The Eclipse Public License is available at
- *    http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at
- *   http://www.eclipse.org/org/documents/edl-v10.php.
- *
- * Contributors:
- *    Ian Craggs - initial API and implementation and/or initial documentation
- *    Ian Craggs - make sure QoS2 processing works, and add device headers
- *******************************************************************************/
 
- /**
-  This is a sample program to illustrate the use of the MQTT Client library
-  on the mbed platform.  The Client class requires two classes which mediate
-  access to system interfaces for networking and timing.  As long as these two
-  classes provide the required public programming interfaces, it does not matter
-  what facilities they use underneath. In this program, they use the mbed
-  system libraries.
+#include "mbed.h"
+#include "stdio.h"
 
- */
-
- // change this to 1 to output messages to LCD instead of serial
-#define USE_LCD 0
-
-#if USE_LCD
-#include "C12832.h"
-
-// the actual pins are defined in mbed_app.json and can be overridden per target
-C12832 lcd(LCD_MOSI, LCD_SCK, LCD_MISO, LCD_A0, LCD_NCS);
-
-#define logMessage lcd.cls();lcd.printf
-
-#else
-
-#define logMessage printf
-
-#endif
+#define logMessage pc.printf
 
 #define MQTTCLIENT_QOS2 1
 
@@ -51,25 +13,63 @@ C12832 lcd(LCD_MOSI, LCD_SCK, LCD_MISO, LCD_A0, LCD_NCS);
 
 int arrivedcount = 0;
 
+Serial pc(USBTX, USBRX);
+PwmOut speaker(p26);
+
+DigitalOut led(LED1);
+
+typedef struct {
+    int frequency;
+} buzzerfreq;
+
+
+Mail<buzzerfreq, 16> mail_box;
 
 void messageArrived(MQTT::MessageData& md)
 {
+    led = !led;
     MQTT::Message &message = md.message;
     logMessage("Message arrived: qos %d, retained %d, dup %d, packetid %d\r\n", message.qos, message.retained, message.dup, message.id);
     logMessage("Payload %.*s\r\n", message.payloadlen, (char*)message.payload);
     ++arrivedcount;
+    
+    buzzerfreq *buzfreq = mail_box.alloc();
+    
+    int test;
+    sscanf(message.payloadlen,(char*)message.payload, "%d", &test);
+    logMessage("Payload INT %i \r\n", test);
+    
+    buzfreq->frequency = test;
+    mail_box.put(buzfreq);    
 }
+
+void playFreqThread(){
+    while (true) {
+        osEvent evt = mail_box.get();
+        if (evt.status == osEventMail) {
+            buzzerfreq *buzfreq = (buzzerfreq*)evt.value.p;
+            printf("\nPlaying frequency: %i\n\r" , buzfreq->frequency);
+            speaker.period((float)(1.0/(float)buzfreq->frequency));  // 4 second period
+            speaker.write(0.50f);  // 50% duty cycle
+            Thread::wait(1000);
+            speaker.write(0.0f);  // 50% duty cycle
+            printf("Done playing...\n\r");
+            mail_box.free(buzfreq);
+        }
+    }
+}
+
 
 
 int main(int argc, char* argv[])
 {
-    float version = 0.6;
-    char* topic = "mbed-sample";
-
-    logMessage("HelloMQTT: version is %.2f\r\n", version);
+    pc.baud(9600);
+    char* topic = "iot/thomas/buzzer";
+    pc.printf("Initing Program");
 
     NetworkInterface* network = easy_connect(true);
     if (!network) {
+        pc.printf("Network failed\r\n");
         return -1;
     }
 
@@ -77,7 +77,7 @@ int main(int argc, char* argv[])
 
     MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
 
-    const char* hostname = "m2m.eclipse.org";
+    const char* hostname = "141.105.126.62";
     int port = 1883;
     logMessage("Connecting to %s:%d\r\n", hostname, port);
     int rc = mqttNetwork.connect(hostname, port);
@@ -86,54 +86,17 @@ int main(int argc, char* argv[])
 
     MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
     data.MQTTVersion = 3;
-    data.clientID.cstring = "mbed-sample";
-    data.username.cstring = "testuser";
-    data.password.cstring = "testpassword";
+    data.clientID.cstring = "thomasWoefBrecht565676";
+
     if ((rc = client.connect(data)) != 0)
         logMessage("rc from MQTT connect is %d\r\n", rc);
 
     if ((rc = client.subscribe(topic, MQTT::QOS2, messageArrived)) != 0)
         logMessage("rc from MQTT subscribe is %d\r\n", rc);
-
-    MQTT::Message message;
-
-    // QoS 0
-    char buf[100];
-    sprintf(buf, "Hello World!  QoS 0 message from app version %f\r\n", version);
-    message.qos = MQTT::QOS0;
-    message.retained = false;
-    message.dup = false;
-    message.payload = (void*)buf;
-    message.payloadlen = strlen(buf)+1;
-    rc = client.publish(topic, message);
-    while (arrivedcount < 1)
+    
+    Thread thread(playFreqThread);
+    
+    while(1){
         client.yield(100);
-
-    // QoS 1
-    sprintf(buf, "Hello World!  QoS 1 message from app version %f\r\n", version);
-    message.qos = MQTT::QOS1;
-    message.payloadlen = strlen(buf)+1;
-    rc = client.publish(topic, message);
-    while (arrivedcount < 2)
-        client.yield(100);
-
-    // QoS 2
-    sprintf(buf, "Hello World!  QoS 2 message from app version %f\r\n", version);
-    message.qos = MQTT::QOS2;
-    message.payloadlen = strlen(buf)+1;
-    rc = client.publish(topic, message);
-    while (arrivedcount < 3)
-        client.yield(100);
-
-    if ((rc = client.unsubscribe(topic)) != 0)
-        logMessage("rc from unsubscribe was %d\r\n", rc);
-
-    if ((rc = client.disconnect()) != 0)
-        logMessage("rc from disconnect was %d\r\n", rc);
-
-    mqttNetwork.disconnect();
-
-    logMessage("Version %.2f: finish %d msgs\r\n", version, arrivedcount);
-
-    return 0;
+    }
 }
